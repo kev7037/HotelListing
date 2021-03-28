5a3099454342174157bb6e5f6a1a0a88360d3e08
AGhasemzad <ali.ghasemzad@gmail.com>
2021-03-28 16:37:55 +0430

    Added DTO clases

---
+++ HotelListing/DTOs/CountryDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.DTOs
+{
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength:200, ErrorMessage ="Country name is too long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 5, ErrorMessage = "Country shortname is too long")]
+        public string ShortName { get; set; }
+    }
+
+    public class CountryDTO: CreateCountryDTO
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ HotelListing/DTOs/HotelDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.DTOs
+{
+    public class CreateHotelDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 200, ErrorMessage = "Hotel name is too long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 1000, ErrorMessage = "Address is too long")]
+        public string Address { get; set; }
+
+        [Range(1, 5)]
+        public double? Rating { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+    }
+    
+    public class HotelDTO: CreateCountryDTO
+    {
+        public int Id { get; set; }
+
+        public CountryDTO Country { get; set; }
+    }
+}
