18d82fba23be6515eed110cb8be8025120746a9b
AGhasemzad <ali.ghasemzad@gmail.com>
2021-03-28 23:45:52 +0430

    Added HotelController

---
+++ HotelListing/Controllers/HotelController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using HotelListing.DTOs;
+using HotelListing.Services.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HotelController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<HotelController> _logger;
+        private readonly IMapper _mapper;
+
+        public HotelController(IUnitOfWork unitOfWork, ILogger<HotelController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotels()
+        {
+            try
+            {
+                var countries = await _unitOfWork.Hotels.GetAll();
+                var results = _mapper.Map<IList<HotelDTO>>(countries);
+                return Ok(countries);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Something went wrong in the {nameof(GetHotels)}");
+                return StatusCode(500, "Internal server error. Try again later.");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotel(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Hotels.Get(expression: q => q.Id == id, new List<string> { "Country" });
+                var results = _mapper.Map<HotelDTO>(country);
+                return Ok(country);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Something went wrong in the {nameof(GetHotel)}");
+                return StatusCode(500, "Internal server error. Try again later.");
+            }
+        }
+
+    }
+}
