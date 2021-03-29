2db1ce178cd949ebc72c1ffee6b8300485711e0b
AGhasemzad <ali.ghasemzad@gmail.com>
2021-03-29 08:24:56 +0430

    Added IdentityCore

---
+++ HotelListing/ServiceExtentions.cs
@@ -0,0 +1,17 @@
+﻿using HotelListing.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelListing
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<ApiUser>(q => q.User.RequireUniqueEmail = true);
+
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+            builder.AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
+        }
+    }
+}
